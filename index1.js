ac0a3641bbbf3631536677438caf3ce3ae3eba47
AbhishekDutta6458 <139978905+AbhishekDutta6458@users.noreply.github.com>
2023-09-28 21:44:19 +0530

    index.js

--- index1.js
+++ index1.js
@@ -1,15 +1,24 @@
+const fetchBtn = document.getElementById("fetch-btn");
+const display = document.getElementById("display");
 
+fetchBtn.addEventListener("click", () => {
+    const xhr = new XMLHttpRequest();
+    
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                const books = JSON.parse(xhr.responseText);
+                books.books.forEach((book) => {
+                    const head = document.createElement("h1");
+                    head.textContent = `${book.name} - ${book.author}`;
+                    display.appendChild(head);
+                });
+            } else {
+                console.error("Failed to fetch data from index.json.");
+            }
+        }
+    };
 
-const fetchBtn= document.getElementById('fetch-btn');
-const display = document.getElementById('display')
-
-fetchBtn.addEventListener('click', async () => {
-
-   const books =  await fetch('index.json').then((res) => res.json())
-   books.books.map((book) => {
-   const head =  document.createElement('h1')
-    head.append(book.name)
-    display.insertAdjacentElement('beforeend', head)
-   })
-})
-
+    xhr.open("GET", "index.json", true);
+    xhr.send();
+});
